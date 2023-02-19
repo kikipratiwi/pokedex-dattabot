02bd0ff54a88ee040b7c8fb92b87ced4b53a8091
Kiki Pratiwi <kikipratiwi@gmail.com>
2023-02-19 10:09:37 +0700

    fix(loading): layout

--- components/atoms/Loading.js
+++ components/atoms/Loading.js
@@ -1,24 +1,19 @@
 import React from "react";
-import { Grid, Skeleton } from "@mui/material";
+import { Grid, Skeleton, Stack } from "@mui/material";
 
 const AtomLoading = () => {
   return (
-    <Grid
-      container
-      rowSpacing={2}
-      columnSpacing={2}
-      columns={24}
-      maxWidth={700}
-      paddingY={2}
-    >
-      <Grid item xs={24} sm={24} md={12} lg={12}>
-        <Skeleton variant="rounded" width={"100%"} height={60} />
-      </Grid>
+    <Stack maxWidth={"700px"} paddingY={2}>
+      <Grid container spacing={2} columns={24}>
+        <Grid item xs={24} sm={24} md={12} lg={12}>
+          <Skeleton variant="rounded" width={"100%"} height={70} />
+        </Grid>
 
-      <Grid item xs={24} sm={24} md={12} lg={12}>
-        <Skeleton variant="rounded" width={"100%"} height={60} />
+        <Grid item xs={24} sm={24} md={12} lg={12}>
+          <Skeleton variant="rounded" width={"100%"} height={70} />
+        </Grid>
       </Grid>
-    </Grid>
+    </Stack>
   );
 };
 
